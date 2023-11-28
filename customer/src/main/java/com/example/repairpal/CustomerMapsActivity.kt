1b2f0d46eef0843d30f1df09995c5f5ff3be98d7
Nyarangi <nyandukonyarangi@gmail.com>
2023-11-28 11:41:23 +0300

    customer changes

--- customer/src/main/java/com/example/repairpal/CustomerMapsActivity.kt
+++ customer/src/main/java/com/example/repairpal/CustomerMapsActivity.kt
@@ -1,87 +1,82 @@
 package com.example.repairpal
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.model.MarkerOptions
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.example.repairpal.databinding.ActivityCustomerMapsBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
-
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.*
 
 class CustomerMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityCustomerMapsBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var lastLocation: Location
+    private lateinit var selectedIssues: ArrayList<String>
+    private lateinit var mechanicsRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_maps)
 
-        binding = ActivityCustomerMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        selectedIssues = intent.getStringArrayListExtra("selectedIssues") ?: arrayListOf()
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-    }
-    companion object{
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
-    }
-
-    private fun placeMarkerOnMap(location: LatLng){
-        val markerOptions = MarkerOptions().position(location)
-        mMap.addMarker(MarkerOptions().position(location).title( "Marker in $location"))
+        mechanicsRef = FirebaseDatabase.getInstance().reference.child("mechanics")
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.uiSettings.isZoomControlsEnabled = true
-        mMap.setOnMarkerClickListener(this)
-        setUpMap()
+
+        displaySelectedIssuesMarkers()
+        fetchMechanicsLocations()
     }
-    private fun setUpMap(){
-        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                LOCATION_PERMISSION_REQUEST_CODE)
-            return
+
+    private fun displaySelectedIssuesMarkers() {
+        selectedIssues.forEach { issue ->
+            val issueLatLng = getLatLngForIssue(issue)
+            placeMarkerOnMap(issueLatLng, issue)
         }
-        mMap.isMyLocationEnabled = true
-        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
-            if(location != null) {
-                lastLocation = location
-                val currentLatLng = LatLng(location.latitude,location.longitude)
-                placeMarkerOnMap(currentLatLng)
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng,12f))
+    }
+
+    private fun getLatLngForIssue(issue: String): LatLng {
+        // Implement your logic to get LatLng for each issue
+        return LatLng(0.0, 0.0) // Replace with actual LatLng
+    }
+
+    private fun placeMarkerOnMap(location: LatLng, issue: String) {
+        val markerOptions = MarkerOptions().position(location).title("$issue location")
+        mMap.addMarker(markerOptions)
+    }
+
+    private fun fetchMechanicsLocations() {
+        mechanicsRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (mechanicSnapshot in dataSnapshot.children) {
+                    val mechanicLocation = LatLng(
+                        mechanicSnapshot.child("latitude").value as Double,
+                        mechanicSnapshot.child("longitude").value as Double
+                    )
+                    placeMechanicMarkerOnMap(mechanicLocation)
+                }
             }
-        }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle database error
+            }
+        })
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-   
+    private fun placeMechanicMarkerOnMap(location: LatLng) {
+        mMap.addMarker(MarkerOptions().position(location).title("Mechanic"))
+    }
+
+    // Other methods and overrides...
 }
 
-private fun GoogleMap.setOnMarkerClickListener(customerMapsActivity: CustomerMapsActivity) {
-
-}
