6c11ff953cb12bd764cb098d1dc277cdc89afa87
Michael Mwai <mwai.michael@strathmore.edu>
2023-11-29 23:55:46 +0300

    Mechanic Module

--- mechanic/build.gradle.kts
+++ mechanic/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mechanic"
+    namespace = "com.example.repairpal"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.mechanic"
+        applicationId = "com.example.repairpal"
         minSdk = 21
         targetSdk = 33
         versionCode = 1
@@ -60,6 +60,8 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-auth")
+    implementation ("com.google.firebase:firebase-database")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -70,6 +72,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
